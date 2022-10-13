11db2caf8bcc8bc6b7f2be0363cfa5c2fbf99557
TheFronk77 <f.patranella@gmail.com>
2022-10-13 17:16:04 -0500

    create flask

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("ay ay tell a shorty come here")
+from urllib import response
+import requests
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def cats():
+
+    url = "https://api.thecatapi.com/v1/images/search?limit=10"
+    response = requests.get (url)
+    json = response.json()
+    url = json[0]["url"]
+    
+    return "<img src='"+url+"'></img>"
+
